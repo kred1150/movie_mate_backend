a4e491b9b74f818227a8d1b3530271b76410bed3
Kevin Redmond <kred1150@gmail.com>
2022-06-16 18:11:53 -0500

    Added an index method to show all movies via the requests page, and added a show method to show a specific movie using user provided params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,14 @@
 class MoviesController < ApplicationController
+  def index
+    response = HTTP.get("https://api.themoviedb.org/3/discover/movie?api_key=#{ENV["TMDB_API_KEY"]}&language=en-US")
+    # poster path = "https://image.tmdb.org/t/p/w1280/#{poster_path}"
+    movies = JSON.parse(response.body)["results"]
+    render json: movies.as_json
+  end
+
+  def show
+    response = HTTP.get("https://api.themoviedb.org/3/movie/#{params[:id]}?api_key=#{ENV["TMDB_API_KEY"]}&language=en-US")
+    movie = JSON.parse(response.body)
+    render json: movie.as_json
+  end
 end
