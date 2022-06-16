8320bf34a43e9cbe5ac32bd639492763cbe20603
Kevin Redmond <kred1150@gmail.com>
2022-06-16 18:41:51 -0500

    installed the bcrypt gem for password_digest, created a User model with username email and password_digest, built a create method in the users controller and tested it to create a dummy user via requests.http

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      username: params[:username],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User successfully created!" }
+    else
+      render json: { errors: user.errors.full_message }
+    end
+  end
 end
