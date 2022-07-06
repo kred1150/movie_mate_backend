08a2df7ae1a36215ec545716245b224d535e6e6c
Kevin Redmond <kred1150@gmail.com>
2022-07-05 20:29:59 -0500

    Added and installed the jb gem, and created an index and show template for RatedMovies that pushes json for the rating, movie and user of each rating.

--- app/controllers/rated_movies_controller.rb
+++ app/controllers/rated_movies_controller.rb
@@ -1,26 +1,26 @@
 class RatedMoviesController < ApplicationController
   def create
-    rated_movie = RatedMovie.new(
+    @rated_movie = RatedMovie.new(
       rating: params[:rating],
       message: params[:message],
       user_id: current_user.id,
       movie_id: params[:movie_id],
     )
-    if rated_movie.save
-      render json: rated_movie.as_json
+    if @rated_movie.save
+      render json: @rated_movie.as_json
     else
-      render json: { errors: rated_movie.errors.full_messages }
+      render json: { errors: @rated_movie.errors.full_messages }
     end
   end
 
   def index
-    rated_movies = RatedMovie.all
-    render json: rated_movies.as_json
+    @rated_movies = RatedMovie.all
+    render template: "rated_movies/index"
   end
 
   def show
-    rated_movie = RatedMovie.find_by(id: params[:id])
-    render json: rated_movie.as_json
+    @rated_movie = RatedMovie.find_by(id: params[:id])
+    render template: "rated_movies/show"
   end
 
   def destroy
