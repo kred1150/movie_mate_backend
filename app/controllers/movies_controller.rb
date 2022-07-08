fcb7b58909d6cd10e2711ee8bfd465064c1c0a96
Kevin Redmond <kred1150@gmail.com>
2022-07-07 19:18:14 -0500

    Changed the Movies Show controller to also pull the cast data for that movie and send that to the front end via customized JSON.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,8 +19,10 @@ class MoviesController < ApplicationController
 
   def show
     movie = Movie.find_by(id: params[:id])
-    response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie["external_id"]}/watch/providers?api_key=#{ENV["TMDB_API_KEY"]}")
-    stream = JSON.parse(response.body)["results"]["US"]
-    render json: { movie: movie.as_json, stream: stream.as_json }
+    stream_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie["external_id"]}/watch/providers?api_key=#{ENV["TMDB_API_KEY"]}")
+    stream = JSON.parse(stream_response.body)["results"]["US"]
+    cast_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie["external_id"]}/credits?api_key=#{ENV["TMDB_API_KEY"]}")
+    cast = JSON.parse(cast_response.body)["cast"]
+    render json: { movie: movie.as_json, stream: stream.as_json, cast: cast.as_json }
   end
 end
