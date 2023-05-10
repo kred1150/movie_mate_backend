640e698a3e1acde7863cc537a8285ebb14a89267
Kevin Redmond <kred1150@gmail.com>
2023-05-10 14:45:12 -0500

    Adjusted the movie show call in MoviesController so that movies will populate for a user based on the external ID even if the movie is not entered into the Heroku Database.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,13 +6,14 @@ class MoviesController < ApplicationController
 
   def show
     movie = Movie.find_by(external_id: params[:id])
-    stream_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie["external_id"]}/watch/providers?api_key=#{ENV["TMDB_API_KEY"]}")
+    movie_id = params[:id]
+    stream_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie_id}/watch/providers?api_key=#{ENV["TMDB_API_KEY"]}")
     stream = JSON.parse(stream_response.body)["results"]["US"]
-    cast_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie["external_id"]}/credits?api_key=#{ENV["TMDB_API_KEY"]}")
+    cast_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie_id}/credits?api_key=#{ENV["TMDB_API_KEY"]}")
     cast = JSON.parse(cast_response.body)["cast"]
-    videos_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie["external_id"]}/videos?api_key=#{ENV["TMDB_API_KEY"]}&language=en-US")
+    videos_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie_id}/videos?api_key=#{ENV["TMDB_API_KEY"]}&language=en-US")
     videos = JSON.parse(videos_response.body)["results"]
-    images_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie["external_id"]}/images?api_key=#{ENV["TMDB_API_KEY"]}")
+    images_response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie_id}/images?api_key=#{ENV["TMDB_API_KEY"]}")
     background_image = JSON.parse(images_response.body)["backdrops"][0]
     render json: { movie: movie.as_json, stream: stream.as_json, cast: cast.as_json, videos: videos.as_json, background_image: background_image.as_json }
   end
